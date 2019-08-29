19ebacad8eb520d64c621f1e7a70c5f94932c727
Matt LaFayette <lafayette.matt@gmail.com>
2019-08-29 13:58:50 -0400

    bar/line chart and acct search

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, g
 import sqlite3
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, NumberRange
 from datetime import time
 
-
+# Login form for index.html
 class MyForm(FlaskForm):
-    name = StringField('Username', validators=[DataRequired()])
+    log_in_name = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+# Search fields for support tickets
+class TicketSearch(FlaskForm):
+	name = StringField('Name', validators=[DataRequired()])
+	address = TextAreaField('Address', validators=[DataRequired()])
+	cx_id = IntegerField('Customer ID', validators=[NumberRange(min=4000000000,max=4009999999)])
 
 app = Flask(__name__)
 app.debug = True
@@ -45,9 +51,30 @@ def submit():
         return redirect('/success')
     return render_template('submit.html', form=form)
 
+@app.route('/ticket', methods=('GET', 'POST'))
+def ticket():
+    form = TicketSearch()
+    return render_template('ticket.html', form=form)
+
+#route for line graph
 @app.route("/simple_chart")
 def chart():
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    legend = 'Clients Activated'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "December"]
     values = [10, 9, 8, 7, 6, 4, 7, 8]
     return render_template('chart.html', values=values, labels=labels, legend=legend)
+
+#pie chart
+@app.route("/pie_chart")
+def pie():
+	activated = 6
+	refused = 1
+	no_answer = 4
+	#probably want to use a dict for some of these values
+	legend = 'Clients Activated'
+	labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "December"]
+	values = [10, 9, 8, 7, 6, 4, 7, 8]
+	ac_label = "red"
+	re_label = "yellow"
+	no_label = "blue"
+	return render_template('pie.html', values=values, labels=labels, legend=legend, activated=activated, refused=refused, no_answer=no_answer)
