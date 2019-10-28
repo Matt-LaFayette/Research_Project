69f6fa174343580a66686a0c770242f5bfaa9be2
Matt LaFayette <lafayette.matt@gmail.com>
2019-10-28 10:42:00 -0400

    heroku fix

--- app/forms.py
+++ app/forms.py
@@ -75,12 +75,13 @@ class RegistrationForm(FlaskForm):
 	role = SelectField(choices=[('sales', 'Sales'), ('support', 'Support'), ('manager', 'Manager')])
 	submit = SubmitField('Register')
 
-	def validate_username(self, username):
-		user = User.query.filter_by(username=username.data).first()
-		if user is not None:
-			raise ValidationError('Please use a different username.')
+	# def validate_username(self, username):
+	# 	user = User.query.filter_by(username=username.data).first()
+	# 	if user is not None:
+	# 		raise ValidationError('Please use a different username.')
 
-	def validate_email(self, email):
-		user = User.query.filter_by(email=email.data).first()
-		if user is not None:
-			raise ValidationError('Please use a different email address.')
+	# def validate_email(self, email):
+	# 	user = User.query.filter_by(email=email.data).first()
+	# 	if user is not None:
+	# 		raise ValidationError('Please use a different email address.')
+
