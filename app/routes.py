3c49f1304cb51183ca1d4fb6f321ebea027c2602
Matt LaFayette <lafayette.matt@gmail.com>
2019-10-28 10:48:50 -0400

    heroku fix

--- app/routes.py
+++ app/routes.py
@@ -124,7 +124,7 @@ def register():
 		return redirect(url_for('index'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		user = User(username=form.username.data, email=form.email.data, role=form.role.data)
+		user = User(username=form.username.data, email=form.email.data, role=str(form.role.data))
 		user.set_password(form.password.data)
 		db.session.add(user)
 		db.session.commit()
